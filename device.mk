52e62cb246ad20ced5eff6a84055416e2b700130
KINGbabasula <kingbabasula@gmail.com>
2015-01-25 10:06:30 +0100

    TWRP updates

--- device.mk
+++ device.mk
@@ -34,3 +34,5 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/recovery/init.recovery.qcom.rc:root/init.recovery.qcom.rc
+
+PRODUCT_COPY_FILES += device/oneplus/bacon/twrp.fstab:recovery/root/etc/twrp.fstab
